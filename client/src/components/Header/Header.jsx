b0f0d45039825b94607d96094a0dd55df61b61b5
georgeromarrr <romar.doinog@gmail.com>
2022-08-11 03:06:48 +0800

    intial header ui

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import bg_desktop from './bg_1920x859.jpg';
+import bg_laptop from './bg_1200x859.jpg';
+import bg_tablet from './bg_770x785.jpg';
+import bg_mobile from './bg_425x785.jpg';
+
+
 
 const Header = () => {
   return (
-    <div className='header-bg'>
-      sad
+    <div>
+      <div>
+        <img className='w-full laptop:hidden tablet:hidden mobile:hidden' src={bg_desktop} alt='1920x859' />
+        <img className='hidden w-full laptop:block tablet:hidden mobile:hidden' src={bg_laptop} alt='1200x859' />
+        <img className='hidden w-full tablet:block mobile:hiden' src={bg_tablet} alt='770x785' />
+        <img className='hidden w-full mobile:block' src={bg_mobile} alt='425x785' />
+
+        
+
+      </div>
     </div>
   )
 }
