71d68ab910622e240936ad313aa0843b92f37110
georgeromarrr <romar.doinog@gmail.com>
2022-08-12 15:52:45 +0800

    Responsive Header UI

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,20 +1,78 @@
-import React from "react";
+import bg_desktop from "../img/header/bg_1920x859.jpg";
+import bg_laptop from "../img/header/bg_1200x859.jpg";
+import bg_tablet from "../img/header/bg_770x785.jpg";
+import bg_mobile from "../img/header/bg_425x785.jpg";
 
-const Header = () => {
+export const HeaderTitle = ({ children }) => {
   return (
     <div>
-      <div className='object-cover w-full h-screen bg-no-repeat bg-[url("/src/components/img/header/bg_1920x859.jpg")]'>
-        {/* <img className='object-cover mix-blend-overlay w-full laptop:hidden tablet:hidden mobile:hidden' src={bg_desktop} alt='1920x859' />
-        <img className='hidden w-full laptop:block tablet:hidden mobile:hidden' src={bg_laptop} alt='1200x859' />
-        <img className='hidden w-full tablet:block mobile:hiden' src={bg_tablet} alt='770x785' />
-        <img className='hidden w-full mobile:block' src={bg_mobile} alt='425x785' /> */}
+      {/* desktop view */}
+      <div className="relative laptop:hidden tablet:hidden mobile:hidden">
+        <img className="w-full" src={bg_desktop} alt="1920x859" />
 
-        <div className="">
-          <h1 className="">Hellow asdasdad</h1>
+        <div className="absolute top-[35%] w-full text-center px-3">
+          <div className="text-white">
+            <h1 className="text-8xl font-bold pb-4">Wanna treat yo' self?</h1>
+            <p className="text-xl">
+              Browse and shop through our Pre-built PC, Peripherals, and
+              Collectibles!
+            </p>
+          </div>
+
+          {children}
+        </div>
+      </div>
+
+      {/* laptop view */}
+      <div className="relative hidden laptop:block tablet:hidden mobile:hidden">
+        <img className="w-full" src={bg_laptop} alt="1200x859" />
+
+        <div className="absolute top-[35%] w-full text-center px-3">
+          <div className="text-white">
+            <h1 className="text-7xl font-bold pb-4">Wanna treat yo' self?</h1>
+            <p className="text-base">
+              Browse and shop through our Pre-built PC, Peripherals, and
+              Collectibles!
+            </p>
+          </div>
+
+          {children}
+        </div>
+      </div>
+
+      {/* tablet view */}
+      <div className="relative hidden tablet:block mobile:hiden">
+        <img className="w-full" src={bg_tablet} alt="770x785" />
+
+        <div className="absolute top-[35%] w-full text-center px-3">
+          <div className="text-white">
+            <h1 className="text-6xl font-bold pb-4">Wanna treat yo' self?</h1>
+            <p className="text-sm">
+              Browse and shop through our Pre-built PC, Peripherals, and
+              Collectibles!
+            </p>
+          </div>
+
+          {children}
+        </div>
+      </div>
+
+      {/* mobile view */}
+      <div className="relative hidden mobile:block">
+        <img className="w-full" src={bg_mobile} alt="425x785" />
+
+        <div className="absolute top-[35%] w-full text-center px-3">
+          <div className="text-white">
+            <h1 className="text-6xl font-bold pb-5">Wanna treat yo' self?</h1>
+            <p className="text-sm">
+              Browse and shop through our Pre-built PC, Peripherals, and
+              Collectibles!
+            </p>
+          </div>
+
+          {children}
         </div>
       </div>
     </div>
   );
 };
-
-export default Header;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,24 @@
-import React from 'react';
-import Navbar from '../../components/Navbar/Navbar';
-import Footer from '../../components/Footer/Footer';
-import Header from '../../components/Header/Header';
-
+import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import Footer from "../../components/Footer/Footer";
+import { HeaderTitle } from "../../components/Header/Header";
 
 const Home = () => {
   return (
     <div>
-        <Navbar/>
-          <Header/>
-        <Footer/>
-    </div>
-  )
-}
+      <Navbar />
+      <HeaderTitle>
+        <button
+          type="button"
+          className="mt-12 text-black bg-white hover:bg-transparent hover:text-white hover:border font-medium text-base px-14 py-4"
+        >
+          SHOP NOW
+        </button>
+      </HeaderTitle>
 
-export default Home
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
